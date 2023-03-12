df6187d9dc09573426b2f63e7a9a1562fb0b5745
Fadi Akkaoui <72225929+FadiAkkaoui@users.noreply.github.com>
2023-03-12 11:02:18 +0100

    project_upload_created_from_spring_initzializr

---
+++ content-calendar/src/test/java/akkaoui/fadi/contentcalendar/ContentCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package akkaoui.fadi.contentcalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
